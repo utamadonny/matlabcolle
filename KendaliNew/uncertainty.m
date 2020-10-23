2a4e7447bf0f445d8c12ae8068d9d28581820b4f
utamadonny <utamadonny10@gmail.com>
2020-10-23 11:08:41 +0700

    use loopsens

--- KendaliNew/uncertainty.m
+++ KendaliNew/uncertainty.m
@@ -29,3 +29,65 @@ sys0= tf([w.NominalValue^2],[1 2*z.NominalValue*w.NominalValue+1 2*z.NominalValu
 norm=sys/sys0 - 1
 W3= makeweight(0,102,-5);
 bodemag(norm, 'b' , W3 ,'r')
+
+%% no1
+clear
+clc
+
+% d = ureal('d',1,'range',[0,0.1]);
+exptheta =  ureal('exptheta',1.0526,'PlusMinus',[-0.0526,0.0526]);
+L = tf(exptheta ,[1 1])
+% L = tf(1,[1 1],'InputDelay',0.1);
+%A = makeweight(0.1,5,0);
+S = 1/1+L;
+T = L/1+L;
+loops = loopsens(T,1);
+Wh = makeweight(0.4,1,20);
+wl = makeweight(10,10,.20);
+%bode(loops.So,'r', loops.To,'b')
+figure(1)
+bodemag(Wh,loops.So)
+figure(2)
+bodemag(wl,loops.To)
+
+%% no2
+clear
+clc
+
+T1 = ureal('T1',1,'range',[0,0.2]);
+T2 = ureal('T2',1,'range',[2,2.5]);
+sys1 = tf(1,[T1 1]);
+sys2 = tf(1,[T2 1]);
+L = sys1*sys2;
+C = 1;
+S = 1/1+L;
+T = L/1+L;
+loops = loopsens(T,C);
+Wh = makeweight(0.4,0.1,10);
+wl = makeweight(10,10,.20);
+%bode(loops.So,'r', loops.To,'b')
+figure(1)
+bodemag(Wh,loops.So)
+figure(2)
+bodemag(wl,loops.To)
+
+%% no3
+clear
+clc
+
+z = ureal('z',1,'range',[0.1,0.2]);
+w = ureal('w',1,'range',[90,110]);
+sys2 = tf(w^2,[1 2*z*w w^2]);
+sys1 = tf(1,[1 1]);
+L = sys1*sys2;
+C = 1;
+S = 1/1+L;
+T = L/1+L;
+loops = loopsens(T,C);
+Wh = makeweight(0.4,3,15);
+wl = makeweight(10,10,.20);
+%bode(loops.So,'r', loops.To,'b')
+figure(1)
+bodemag(Wh,loops.So)
+figure(2)
+bodemag(wl,loops.To)
